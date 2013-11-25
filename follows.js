fc63297986d382386364edd55f53f4d93ec9e79f
ltc-chris-gass <cgass@linear.com>
2013-11-25 14:43:38 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'O9XRb2kSy66cauj04ve642R4rdoXf6YyVjpuscaG2PpZngPA', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [956611514], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Chris Gass Demo', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
